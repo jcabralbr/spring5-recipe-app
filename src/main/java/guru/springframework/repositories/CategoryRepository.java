0397efc979a9d89b3599a7d6854d3ca4ef08b929
Julio Cesar Cabral <jcabbralbr@gmail.com>
2018-12-14 12:31:17 -0200

    Using setters for JPA Bidirectional Relationships

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,11 +1,11 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface CategoryRepository extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
 
     Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,11 +1,11 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.UnitOfMeasure;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+public interface UnitOfMeasureRepository extends PagingAndSortingRepository<UnitOfMeasure, Long> {
 
     Optional<UnitOfMeasure> findByDescription(String description);
 
