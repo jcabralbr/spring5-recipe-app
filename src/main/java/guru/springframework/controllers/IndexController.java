94c7f43df8210f599303e9a3df53575678f31d34
Julio Cesar Cabral <jcabbralbr@gmail.com>
2018-12-27 11:49:02 -0200

    creating test to IndexController

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+
 @Slf4j
 @Controller
 public class IndexController {
